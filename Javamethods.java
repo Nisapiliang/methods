988a59f3d76a248b902106ce1834fa56790eb26f
Nisapiliang <khairunnisa.kn369@gmail.com>
2021-10-18 13:07:49 +0700

    first commit

---
+++ Javamethods.java
@@ -0,0 +1,30 @@
+public class Javamethods{
+
+    
+        static void myMethod(int nilai) {
+      
+            if(nilai< 25){
+                System.out.println(" Nilai Anda: E");
+            } else if ( nilai < 50){
+                System.out.println("Nilai Anda: D");
+            } else if ( nilai < 75){
+                System.out.println("Nilai Anda: C");
+            } else if ( nilai < 90){
+                System.out.println("Nilai Anda: B");
+            } else if (nilai < 100){
+                System.out.println("Nilai Anda: A");   
+            } else if (nilai> 100) {
+                System.out.println("Invalid Number");
+               
+            } 
+        }  
+      
+        public static void main(String[] args) {
+          myMethod(10);
+          return;
+         
+        }
+    }
+    
+
+    
